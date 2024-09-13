b15a8a9ff7752fbb174118ee46bbaac1b1a2517b
rustutam <Rustam.Asbapov@urfu.me>
2024-09-13 18:35:53 +0500

    feat: добавил методы для получения категории и сложности в ConsoleInputProvider

--- src/main/java/backend/academy/Game/ConsoleInputProvider.java
+++ src/main/java/backend/academy/Game/ConsoleInputProvider.java
@@ -1,9 +1,22 @@
 package backend.academy.Game;
 
+import java.util.List;
+
 public class ConsoleInputProvider implements InputProvider {
     @Override
     public String getInput() {
-        System.out.println("Введите букву: ");
+        System.out.print("Введите букву: ");
+        return System.console().readLine();
+    }
+
+    public String getInputCategory(List<String> categories) {
+        System.out.print("Выберите категорию: " + categories);
+        return System.console().readLine();
+
+    }
+
+    public String getInputLevel(List<String> levels) {
+        System.out.println("Выберите уровень сложности: " + levels);
         return System.console().readLine();
     }
 }
