437be36ccff9438152859d6a6a8297623c72a2da
rustutam <Rustam.Asbapov@urfu.me>
2024-09-15 13:37:47 +0500

    fix: изменил поток ввода на стандартный(Scanner)

--- src/main/java/backend/academy/Game/ConsoleInputProvider.java
+++ src/main/java/backend/academy/Game/ConsoleInputProvider.java
@@ -1,22 +1,24 @@
 package backend.academy.Game;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class ConsoleInputProvider implements InputProvider {
+    Scanner scanner = new Scanner(System.in);
     @Override
     public String getInput() {
         System.out.print("Введите букву: ");
-        return System.console().readLine();
+        return scanner.nextLine();
     }
 
     public String getInputCategory(List<String> categories) {
         System.out.print("Выберите категорию: " + categories);
-        return System.console().readLine();
+        return scanner.nextLine();
 
     }
 
     public String getInputLevel(List<String> levels) {
         System.out.println("Выберите уровень сложности: " + levels);
-        return System.console().readLine();
+        return scanner.nextLine();
     }
 }
