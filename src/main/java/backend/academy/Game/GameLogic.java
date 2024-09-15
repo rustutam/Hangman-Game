9d0fc703499f2e129a2d9d8ee1bfbb97be4caa6b
rustutam <Rustam.Asbapov@urfu.me>
2024-09-15 16:07:52 +0500

    feat: добавил переменную, для отслеживания состояния виселицы

--- src/main/java/backend/academy/Game/GameLogic.java
+++ src/main/java/backend/academy/Game/GameLogic.java
@@ -6,13 +6,16 @@ import java.util.HashSet;
 
 public class GameLogic {
     private String word;
+    private final int MAX_ATTEMPTS;
     private Integer attempts;
     private GameStatus gameStatus = GameStatus.PLAYING;
     private String hiddenWord;
     private HashSet<String> usedSymbolsSet = new HashSet<>();
+    private Integer gallowsState = 0;
 
     public GameLogic(String word, Integer attempts) {
         this.word = word;
+        this.MAX_ATTEMPTS = attempts;
         this.attempts = attempts;
         hiddenWord = "_".repeat(word.length());
     }
@@ -47,6 +50,8 @@ public class GameLogic {
         if (attempts <= 0) {
             gameStatus = GameStatus.LOSE;
         }
+
+        gallowsState =(int) Math.floor(((double) (MAX_ATTEMPTS-attempts) /MAX_ATTEMPTS)*7.0);
     }
 
     public HashSet<String> getUsedSymbolsSet() {
@@ -63,6 +68,10 @@ public class GameLogic {
 
     public String getHiddenWord() {
         return hiddenWord;
+    }
+
+    public Integer getGallowsState() {
+        return gallowsState;
     }
 
     public String getGameInformation() {
